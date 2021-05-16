0cc192431684e440e03c9609d7ccd87b985ce895
Josh McLain <joshmclain45@gmail.com>
2021-05-16 15:34:12 -0700

    navbar layout and styling compleeted, still needs some functionality

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,39 @@
+import React, {Fragment} from 'react'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements'
+import { FaBars } from 'react-icons/fa'
+
+const Navbar = () => {
+    return (
+        <Fragment>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>
+                        Dolla
+                    </NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>about</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='discover'>discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='services'>services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='signup'>sign up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </Fragment>
+    )
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
 const target = document.getElementById('root')
 
-ReactDOM.render(<h1>Hello world</h1>, target);
+ReactDOM.render(<App />, target);
 
 
