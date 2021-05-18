ee3ad1e6ef3608c3cb06a062a0e0ac02c8cc20e3
Josh McLain <joshmclain45@gmail.com>
2021-05-18 13:05:51 -0700

    added in content for data to pass as props for InfoSection components

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -35,19 +35,19 @@ export const homeObjTwo = {
 };
 
 export const homeObjThree = {
-  id: "about",
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: "Premium Bank",
-  headline: "Unlimited Transactions with zero fees",
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our Team",
+  headline: "Creating an account is extremely easy",
   description:
-    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
-  buttonLabel: "Get started",
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start Now ",
   imgStart: false,
   img: require("../../images/data.svg"),
   alt: "Car",
-  dark: true,
-  primary: true,
-  darkText: false,
+  dark: false,
+  primary: false,
+  darkText: true,
 };
