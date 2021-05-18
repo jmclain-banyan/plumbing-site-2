e9a990adcf5ccd9b911292b4c910f361568806c0
Josh McLain <joshmclain45@gmail.com>
2021-05-18 01:15:01 -0700

    quick host test

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -15,3 +15,39 @@ export const homeObjOne = {
   primary: true,
   darkText: false,
 };
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/piggyBank.svg"),
+  alt: "Piggy Bank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/data.svg"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
