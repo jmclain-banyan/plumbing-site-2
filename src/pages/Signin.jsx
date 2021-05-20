90321e629f9aa122902a33f79d8bcad30c601709
Josh McLain <joshmclain45@gmail.com>
2021-05-19 20:22:27 -0700

    completed Footer component, added in Social Media element

---
+++ src/pages/Signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import styled from 'styled-components'
+
+const Signin = () => {
+    return (
+        <div>
+            <h1>sign in page</h1>
+        </div>
+    )
+}
+
+export default Signin
